20d6e799d6fb18b6b3c8b72cfb09ae33a7a96345
Nyiri Levente <nyiri.levi88@gmail.com>
2024-05-19 18:49:52 +0200

    Added IoT client app

---
+++ IoT_ImageDetection/IotGUI/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace IotGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Cursor = Cursors.No;
+            textBox1.Location = new Point(69, 384);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(650, 31);
+            textBox1.TabIndex = 0;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(69, 42);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(650, 336);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Name = "MainForm";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ IoT_ImageDetection/IotGUI/MainForm.cs
@@ -0,0 +1,97 @@
+namespace IotGUI
+{
+    using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+    using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+    using Microsoft.Rest;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            // Configure the PictureBox
+            pictureBox1.Location = new Point(0, 0); // Start at the top-left corner of the form
+            pictureBox1.Width = this.ClientSize.Width; // Stretch across the width of the form
+            pictureBox1.Height = (int)(this.ClientSize.Height * 0.8); // Take 80% of the form's height
+
+            // Configure the TextBox
+            textBox1.Location = new Point(0, pictureBox1.Height); // Start right below the PictureBox
+            textBox1.Width = this.ClientSize.Width; // Stretch across the width of the form
+            textBox1.Height = this.ClientSize.Height - pictureBox1.Height; // Take the remaining height
+
+
+            // Make sure the controls resize correctly when the form is resized
+            this.Resize += MainForm_Resize;
+            this.ActiveControl = pictureBox1;
+            Task.Run(() => ImageProcessing());
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            // Adjust the width and height on form resize
+            pictureBox1.Width = this.ClientSize.Width; // Adjust width to form size
+            pictureBox1.Height = (int)(this.ClientSize.Height * 0.8); // Adjust height to 80% of form height
+
+            textBox1.Location = new Point(0, pictureBox1.Height); // Move below PictureBox
+            textBox1.Width = this.ClientSize.Width; // Adjust width to form size
+            textBox1.Height = this.ClientSize.Height - pictureBox1.Height; // Adjust remaining height
+        }
+
+        public async Task ImageProcessing()
+        {
+            string endpoint = "https://computer-vision-demo-karimi.cognitiveservices.azure.com/";
+            string subscriptionKey = "47ece2c929af4236b3f3a48ecdce1436";
+            string imageUrl = "http://192.168.68.110/capture";
+
+            var credentials = new ApiKeyServiceClientCredentials(subscriptionKey);
+            var client = new ComputerVisionClient(credentials)
+            {
+                Endpoint = endpoint
+            };
+
+            HttpClient httpClient = new HttpClient();
+
+            try
+            {
+                while (true)
+                {
+                    byte[] imageBytes = await httpClient.GetByteArrayAsync(imageUrl);
+                    using var imageStream = new MemoryStream(imageBytes);
+                    imageStream.Position = 0; // Reset position after reading
+                    Image imageFromStream = Image.FromStream(imageStream);
+
+                    // Reset stream position again for Azure analysis
+                    imageStream.Position = 0;
+                    var features = new List<VisualFeatureTypes?> { VisualFeatureTypes.Description };
+                    var analysisResult = await client.AnalyzeImageInStreamAsync(imageStream, features);
+
+                    if (analysisResult.Description.Captions.Count > 0)
+                    {
+                        var caption = analysisResult.Description.Captions[0];
+                        Invoke(new Action(() => {
+                            textBox1.Text = $"Caption: '{caption.Text}', Confidence: {caption.Confidence:F2}";
+                            pictureBox1.Image = imageFromStream;
+                        }));
+
+                    }
+
+                    }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                MessageBox.Show("Error during the image processing.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+}
